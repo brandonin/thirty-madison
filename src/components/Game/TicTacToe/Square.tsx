4a348a796d319288945ca2a85a9a091421021120
Brandon In <brandonin@me.com>
2020-07-01 10:09:09 -0700

    removed unused variables

--- src/components/Game/TicTacToe/Square.tsx
+++ src/components/Game/TicTacToe/Square.tsx
@@ -1,33 +1,35 @@
-import React, { MouseEvent } from "react";
+import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 
-import { GetGame_game_board_squares } from "../../../graphql/__generated__/GetGame";
+import { GetGame_game_board_squares } from '../../../graphql/__generated__/GetGame';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         paper: {
             height: 200,
             width: 200,
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
             color: theme.palette.primary.main,
-            cursor: "pointer",
-        }
+            cursor: 'pointer',
+        },
     }),
 );
 
 interface IClickedComponent {
-    value: string
+    value: string;
 }
 
 const ClickedComponent: React.FC<IClickedComponent> = ({ value }) => {
     return (
-        <Typography variant="h1" align="center">{value}</Typography>
-    )
-}
+        <Typography variant="h1" align="center">
+            {value}
+        </Typography>
+    );
+};
 
 interface ISquare {
     square: GetGame_game_board_squares;
@@ -37,12 +39,12 @@ interface ISquare {
 // Take in the current user and fill it with either an X or an O.
 const Square: React.FC<ISquare> = ({ square, onClick }) => {
     const classes = useStyles();
-    
+
     const handleClick = () => {
         onClick(square);
-    }
+    };
     return (
         <Paper className={classes.paper} onClick={handleClick} children={<ClickedComponent value={square?.value} />} />
     );
-}
+};
 export default Square;
